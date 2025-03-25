Added change to flip signs in progress evaluations so that the colors look correct - NOTE still some color issue might need to fix later

utils/progress_evaluation.py
@@ -134,8 +134,9 @@ class VisualizeSafeSet2D(EvaluationMetric):
 
                 with torch.no_grad():
                     values = model_eval(coords)
-                    if not self.isHJR:
-                        values = - values # Flip to line up conventions: positive is safe 
+                    if ((not self.isHJR and (self.dataset.dynamics.loss_type == "brat_hjivi" or self.dataset.dynamics.loss_type == "brat_hjivi_ci")) or \
+                         (self.dataset.dynamics.loss_type == "brt_hjivi" and self.dataset.dynamics.set_mode == "reach")): 
+                        values = - values # Flip to line up conventions: positive is safe - for reach avoid or only reach problems
                     sdf_values = self.dataset.dynamics.boundary_fn(coords[:, 1:].to(values.device))
                     if self.dataset.dynamics.loss_type == 'brat_hjivi':
                         avoid_values = self.dataset.dynamics.avoid_fn(coords[:, 1:].to(values.device))