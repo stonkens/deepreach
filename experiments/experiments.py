693ebac276f502cb88128a397a496554bb81555c
Sander Tonkens <stonkens@ucsd.edu>
2025-04-19 23:40:14 -0700

    more batch experiment friendly

--- experiments/experiments.py
+++ experiments/experiments.py
@@ -622,7 +622,7 @@ class Experiment(ABC):
                         if CSL_val_loss < CSL_loss_frac_cutoff*CSL_initial_val_loss:
                             break
 
-                if not (epoch+1) % epochs_til_checkpoint:
+                if (not (epoch+1) % epochs_til_checkpoint) or (epoch + 1) == 1:
                     # Saving the optimizer state is important to produce consistent results
                     checkpoint = { 
                         'epoch': epoch+1,
